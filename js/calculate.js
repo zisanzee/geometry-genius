c3bd669db835630b9e3250b2501a20c078490582
Towhid Hossain <zisankhanchowdhury@gmail.com>
2023-08-18 15:51:44 +0600

    finished for now

--- js/calculate.js
+++ js/calculate.js
@@ -11,7 +11,8 @@ document.getElementById('tri-calc').addEventListener('click', function(e){
   resultArea.innerText = `
   Triangle
 Area (A) = 0.5 x b x h 
-0.5 x ${el[0]} x ${el[1]} = ${result}`
+0.5 x ${el[0]} cm x ${el[1]} cm = ${result} cm
+`
 
 });
 document.getElementById('rect-calc').addEventListener('click', function(e){
@@ -20,7 +21,7 @@ document.getElementById('rect-calc').addEventListener('click', function(e){
   resultArea.innerText = `
   Rectangle
 Area (A) = w x i
- ${el[0]} x ${el[1]} = ${result}`
+ ${el[0]} cm x ${el[1]} cm = ${result} cm`
 })
 
 document.getElementById('para-calc').addEventListener('click', function(e){
@@ -37,5 +38,22 @@ document.getElementById('rhom-calc').addEventListener('click', function(e){
   resultArea.innerText = `
   Rectangle
 Area (A) = .5 x d1 x d2
- ${el[0]} x ${el[1]} = ${result}`
+ ${el[0]} cm x ${el[1]} cm = ${result} cm`
+})
+
+document.getElementById('penta-calc').addEventListener('click', function(e){
+  let el = getEl(e);
+  let result = .5 * el[0] * el[1];
+  resultArea.innerText = `
+  Rectangle
+Area (A) = .5 x p x b
+ ${el[0]} cm x ${el[1]} cm = ${result} cm`
+})
+document.getElementById('eli-calc').addEventListener('click', function(e){
+  let el = getEl(e);
+  let result = Math.PI * el[0] * el[1];
+  resultArea.innerText = `
+  Rectangle
+Area (A) =  π ab
+π x ${el[0]} cm x ${el[1]} cm = ${result} cm`
 })
