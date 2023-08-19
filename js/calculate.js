87f1eae582a9aeec9a5250c7eda829a863ce053b
Towhid Hossain <zisankhanchowdhury@gmail.com>
2023-08-19 15:37:41 +0600

    changed functionality

--- js/calculate.js
+++ js/calculate.js
@@ -1,59 +1,77 @@
 const resultArea = document.getElementById('result');
+let count = 0;
 function getEl(e) {
   const b = e.target.parentNode.parentNode.children[2].children[0].value;
   const h = e.target.parentNode.parentNode.children[2].children[2].value;
   let el = [b,h]
   return el
 };
+
+function saveResult (el, resultText){
+  let div = document.createElement('div');
+div.innerHTML = `
+  <p> ${count}. ${el.target.parentNode.parentNode.children[0].innerText}</p>
+  <p> ${resultText}</p>
+  <button class="btn" onclick="resultArea.removeChild(this.parentNode)"> clear </button>
+`;
+div.children[0].style.fontWeight = 'bold';
+resultArea.appendChild(div);
+}
+
 document.getElementById('tri-calc').addEventListener('click', function(e){
+  count++;
   let el = getEl(e);
   let result = 0.5 * el[0] * el[1];
-  resultArea.innerText = `
-  Triangle
-Area (A) = 0.5 x b x h 
-0.5 x ${el[0]} cm x ${el[1]} cm = ${result} cm
-`
+let resultText = ` 0.5 x ${el[0]} cm x ${el[1]} cm = ${result} cm²`
+e.target.parentNode.parentNode.children[3].innerText = resultText;
+
+saveResult(e, resultText);
+
+
 
 });
 document.getElementById('rect-calc').addEventListener('click', function(e){
+  count++;
   let el = getEl(e);
   let result = el[0] * el[1];
-  resultArea.innerText = `
-  Rectangle
-Area (A) = w x i
- ${el[0]} cm x ${el[1]} cm = ${result} cm`
+  let resultText = `
+  ${el[0]} cm x ${el[1]} cm = ${result} cm²`
+  e.target.parentNode.parentNode.children[3].innerText = resultText;
+  saveResult(e, resultText);
 })
 
 document.getElementById('para-calc').addEventListener('click', function(e){
+  count++;
   let el = getEl(e);
   let result = el[0] * el[1];
-  resultArea.innerText = `
-  Rectangle
-Area (A) = b x h
- ${el[0]} x ${el[1]} = ${result}`
+  let resultText = `
+  ${el[0]} cm x ${el[1]} cm = ${result} cm²`
+  e.target.parentNode.parentNode.children[3].innerText = resultText;
+  saveResult(e, resultText);
 })
 document.getElementById('rhom-calc').addEventListener('click', function(e){
+  count++;
   let el = getEl(e);
   let result = .5 * el[0] * el[1];
-  resultArea.innerText = `
-  Rectangle
-Area (A) = .5 x d1 x d2
- ${el[0]} cm x ${el[1]} cm = ${result} cm`
+  let resultText = `
+  ${el[0]} cm x ${el[1]} cm = ${result} cm²`;
+  e.target.parentNode.parentNode.children[3].innerText = resultText;
+  saveResult(e, resultText);
 })
 
 document.getElementById('penta-calc').addEventListener('click', function(e){
+  count++;
   let el = getEl(e);
   let result = .5 * el[0] * el[1];
-  resultArea.innerText = `
-  Rectangle
-Area (A) = .5 x p x b
- ${el[0]} cm x ${el[1]} cm = ${result} cm`
+  let resultText = `${el[0]} cm x ${el[1]} cm = ${result} cm²`
+  e.target.parentNode.parentNode.children[3].innerText = resultText;
+  saveResult(e, resultText);
 })
 document.getElementById('eli-calc').addEventListener('click', function(e){
+  count++;
   let el = getEl(e);
   let result = Math.PI * el[0] * el[1];
-  resultArea.innerText = `
-  Rectangle
-Area (A) =  π ab
-π x ${el[0]} cm x ${el[1]} cm = ${result} cm`
+  let resultText = `π x ${el[0]} cm x ${el[1]} cm = ${result} cm²`;
+  e.target.parentNode.parentNode.children[3].innerText = resultText;
+  saveResult(e, resultText);
 })
